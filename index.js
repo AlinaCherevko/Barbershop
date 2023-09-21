23a9df32fb6e65cd2548050544f385725edc9cee
Alina Cherevko <alinapavluhina@ukr.net>
2023-09-21 12:03:20 +0300

    change mob-menu effect

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const burgerBtnEl = document.querySelector(".open-mob-menu");
 const closeBtn = document.querySelector(".close-mob-menu");
 const mobMenu = document.querySelector(".mob-menu");
+const aboutEl = document.getElementById("about-mob");
+const servicesEl = document.getElementById("servises-mob");
+const barbersEl = document.getElementById("barbers-mob");
+const contactsEl = document.getElementById("contacts-mob");
+const bookingEl = document.getElementById("booking-mob");
 
 burgerBtnEl.addEventListener("click", onBurgerClick);
 closeBtn.addEventListener("click", onCloseClick);
@@ -10,4 +15,11 @@ function onCloseClick() {
 }
 function onBurgerClick() {
   mobMenu.classList.remove("is-hidden");
+}
+
+mobMenu.addEventListener("click", onLinkClick);
+
+function onLinkClick() {
+  if ((target = aboutEl || servicesEl || barbersEl || contactsEl || bookingEl))
+    onCloseClick();
 }
